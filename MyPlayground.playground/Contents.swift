c2f6e4f2720ecf9630968d7ed36d5d6f5d9c42e2
Apple <apple@Apples-MacBook-Pro-2.local>
2019-08-06 07:43:31 -0700

    First commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var str = "Hello, playground"
+//
+
+//interpolation example
+let name = "valeria"
+print("hi there \(name)")
+
+//let = for constants
+//var = info that can change
+
+var favUniversity = "NYU"
+print ("I love \(favUniversity)")
+favUniversity = "Stony Brook University"
+print ("I also love \(favUniversity) too!")
+let favCity = "New York, New York"
+print("My favorite schools are located in \(favCity)")
